e2a79607df7b457f3e199935d634371f3486a18a
dedybayu <dedybayusetiawan5@gmail.com>
2025-12-14 21:59:55 +0700

    update authorisasi prodsuk

--- routes/produk_routes.go
+++ routes/produk_routes.go
@@ -12,16 +12,16 @@ func SetupProdukRoutes(api *gin.RouterGroup, produkController *controllers.Produ
 	produk := api.Group("/produk")
 	{
 		// Public routes (butuh auth)
-		produk.GET("", authMiddleware.RequireLevel(1, 2), produkController.GetAllProduk)
-		produk.GET("/terbaru", authMiddleware.RequireLevel(1, 2), produkController.GetProdukTerbaru)
-		produk.GET("/stok-menipis", authMiddleware.RequireLevel(1, 2), produkController.GetProdukStokMenipis)
-		produk.GET("/statistik", authMiddleware.RequireLevel(1, 2), produkController.GetStatistikProduk)
-		produk.GET("/:id", authMiddleware.RequireLevel(1, 2), produkController.GetProdukByID)
+		produk.GET("", authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6), produkController.GetAllProduk)
+		produk.GET("/terbaru", authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6), produkController.GetProdukTerbaru)
+		produk.GET("/stok-menipis", authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6), produkController.GetProdukStokMenipis)
+		produk.GET("/statistik", authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6), produkController.GetStatistikProduk)
+		produk.GET("/:id", authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6), produkController.GetProdukByID)
 		produk.GET("/image/:filename", produkController.GetProdukFoto)
-		
+
 		// Admin only routes
 		adminProduk := produk.Group("")
-		adminProduk.Use(authMiddleware.RequireLevel(1))
+		adminProduk.Use(authMiddleware.RequireLevel(1, 2, 3, 4, 5, 6))
 		{
 			adminProduk.POST("", produkController.CreateProduk)
 			adminProduk.PUT("/:id", produkController.UpdateProduk)
